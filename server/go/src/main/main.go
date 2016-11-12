c0bcaf86b4b9d8eb0677da9569f5c933b414dabf
Christopher Tham <chriswt91@gmail.com>
2016-11-11 16:30:15 -0800

    (feat) Initialize basic web-server with `/test` route

--- server/go/src/main/main.go
+++ server/go/src/main/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+  "fmt" // Formatting library
+  "net/http" // HTTP request handler
+  "github.com/julienschmidt/httprouter" // Third-party routing middleware
+)
 
-func main() {  
-  fmt.Println("Hello, Gopher!")
+func main() {
+
+  // Instantiate a new router
+  r := httprouter.New()
+
+  // `/test` GET route for testing
+  r.GET("/test", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+    fmt.Fprint(w, "Welcome!\n")
+  })
+
+  http.ListenAndServe("localhost:3000", r)
 }
