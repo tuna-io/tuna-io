083eda8362d612ea0957655ba364640e026827ac
Christopher Tham <chriswt91@gmail.com>
2016-11-12 14:26:55 -0800

    (feat) Define video schema

--- server/src/models/videosModel.go
+++ server/src/models/videosModel.go
@@ -1,6 +1,17 @@
-package videoSchema
+package videosSchema
+
+import "time"
 
 type Video struct {
-  Name string
-  Age int
+  Id        int       `json:"id"`
+  Title     string    `json:"title"`
+  Url       string    `json:"url"`
+  Hash      string    `json:"hash"`
+  Author_id int       `json:"author_id"`
+  Timestamp time.Time `json:"timestamp"`
+  Private   bool      `json:"private"`
+  Likes     []int     `json:"likes"`
+  Dislikes  []int     `json:"dislikes"`
 }
+
+type Videos []Video
