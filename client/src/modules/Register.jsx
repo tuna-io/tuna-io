e6583860acee523d783d18f177fbf8f6d8d4d3cc
Christopher Tham <chriswt91@gmail.com>
2016-11-17 15:28:36 -0800

    (feat) Update onChange handler and implement submit handler

--- client/src/modules/Register.jsx
+++ client/src/modules/Register.jsx
@@ -3,29 +3,45 @@ import React from 'react';
 export default class Register extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       'username': '',
       'email': '',
       'password': ''
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert(this.state.username);
+    event.preventDefault();
   }
 
   render() {
     return (
       <div>
         <div>Register for an account in under a minute!</div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div>
             Username:
-            <input type="text" value={this.state.username} onChange={() => this.setState("username")} />
+            <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
           </div>
           <div>
             Email:
-            <input type="email" value={this.state.email} onChange={() => this.setState("email")} />
+            <input type="email" value={this.state.email} onChange={this.handleChange} />
           </div>
           <div>
             Password:
-            <input type="password" value={this.state.password} onChange={() => this.setState("password")} />
+            <input type="password" value={this.state.password} onChange={this.handleChange} />
+          </div>
+          <div>
+            <input type="submit" value="Submit" />
           </div>
         </form>
       </div>
