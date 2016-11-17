faf96a1708594c341e30eeb63fb225d65635089a
Christopher Tham <chriswt91@gmail.com>
2016-11-17 14:45:13 -0800

    (feat) Convert registration to stateful component

--- client/src/modules/Register.jsx
+++ client/src/modules/Register.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-export default React.createClass({
+export default class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'username': '',
+      'email': '',
+      'password': ''
+    };
+  }
+
   render() {
     return (
       <div>This is the registration page</div>
     )
   }
-});
+};
