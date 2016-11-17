824deb9f3e3f9b5206e5eed26f457f53c6873d18
Christopher Tham <chriswt91@gmail.com>
2016-11-17 14:23:57 -0800

    (refactor) Convert hashHistory to browserHistory
    - Makes ugly /#/ disappear in URLs

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import './index.css';
 import App from './modules/App';
 import About from './modules/About';
@@ -9,18 +9,25 @@ import Dashboard from './modules/Dashboard';
 import Settings from './modules/Settings';
 import Upload from './modules/Upload';
 import VideoDetails from './modules/VideoDetails'
+import Home from './modules/Home';
 
 ReactDOM.render((
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App} >
-
       <Route path="/about" component={About} />
       <Route path="/signin" component={Signin} />
       <Route path="/dashboard" component={Dashboard} >
         <Route path="/dashboard/settings" component={Settings} />
         <Route path="/dashboard/upload" component={Upload} />
+      <IndexRoute component={Home} />
+      <Route path="about" component={About} />
+      <Route path="repos" component={Repos} />
+      <Route path="signin" component={Signin} />
+      <Route path="dashboard" component={Dashboard} >
+        <Route path="dashboard/settings" component={Settings} />
+        <Route path="dashboard/upload" component={Upload} />
       </Route>
       <Route path="/videos/:videoId" component={VideoDetails} />
     </Route>
   </Router>
-  ), document.getElementById('root'));
+), document.getElementById('root'));
