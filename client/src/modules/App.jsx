20ff56b8b162497b957449be68c37aeb8884ba77
Christopher Tham <chriswt91@gmail.com>
2016-11-18 21:15:36 -0800

    (feat) Home component resolves session if !loggedIn

--- client/src/modules/App.jsx
+++ client/src/modules/App.jsx
@@ -54,7 +54,7 @@ export default class App extends React.Component {
               auth: this.authenticateUser,
             }
           ) || 
-          <Home loggedIn={this.state.loggedIn} />
+          <Home loggedIn={this.state.loggedIn} auth={this.authenticateUser} />
         }
       </div>
     )
