6c0d0fd9902b02d81ad22040a85f17c38c27a6ec
Christopher Tham <chriswt91@gmail.com>
2016-11-18 19:16:02 -0800

    (feat) Add authenticate handler to App

--- client/src/modules/App.jsx
+++ client/src/modules/App.jsx
@@ -5,6 +5,33 @@ import Home from './Home';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      'loggedIn': '',
+    };
+  }
+
+  authenticateUser() {
+    fetch('http://127.0.0.1:3000/api/users/authenticate', {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(jsonResponse) {
+      console.log(jsonResponse);
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+  }
+
+  componentDidMount() {
+    this.state.loggedIn ? this.authenticateUser() : null;
   }
 
   render() {
