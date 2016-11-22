d2945c85b6205bac1b8244f421be66e6738818f1
Christopher Tham <chriswt91@gmail.com>
2016-11-22 14:58:43 -0800

    (feat) Instantiate API server

---
+++ static-server/api.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/julienschmidt/httprouter"
+)
+
+const (
+	VERSION = 1
+)
+
+// Add api endpoints in here, you are allowed to use httprouter syntax to define parameters
+func (s *Server) DefineEndpoints() {
+	s.Endpoint("version", API_GET|API_POST, VersionEndpoint)
+}
+
+func VersionEndpoint(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	ver := struct {
+		Version float64 `json:"ver"`
+	}{
+		VERSION,
+	}
+	log.Infoln(req.RemoteAddr, req.Method, "version api")
+	data, _ := json.Marshal(ver)
+	//w.Header().Add("Content-type", "encoding/json")
+
+	w.Write(data)
+}
