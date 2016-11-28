cccc86d37f08d49172b21bba8e36cd30b2eedcff
Bobby Wei <showmethewei@gmail.com>
2016-11-28 11:22:59 -0800

    Add button to toggle edit mode. Attach a handler

--- client/src/modules/Transcript.js
+++ client/src/modules/Transcript.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
 class Transcript extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inEditMode: false,
+    };
+
+    this.changeMode = this.changeMode.bind(this);
+  }
+
+  changeMode(event) {
+    this.setState({ inEditMode: event.target.checked });
+  }
+
   render() {
     return (
       <div>
-        {this.props.transcript.map(pair => pair.word).reduce((firstword, secondword) => `${firstword} ${secondword}`)}
+        <span>Edit mode: </span><input type="checkbox" onChange={this.changeMode} />
+        <div>
+          {
+            !this.state.inEditMode ?
+            this.props.transcript.map(pair => pair.word).reduce((firstword, secondword) => `${firstword} ${secondword}`) : null
+          }
+        </div>
       </div>
     );
   }
