175f0b6807db1dce5c600781b969c21bd2bb65bd
Christopher Tham <chriswt91@gmail.com>
2016-11-30 17:14:54 -0800

    (feat) Optimize query retrieval

--- client/src/modules/Search.js
+++ client/src/modules/Search.js
@@ -8,10 +8,11 @@ export default class Search extends React.Component {
       query: props.params.query,
     };
 
+    this.renderSearchResults(props.params.query);
   }
 
-  componentDidMount() {
-    fetch(`/api/search/videos/${this.state.query}`, {
+  renderSearchResults(query) {
+    return fetch(`/api/search/videos/${query}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -25,15 +26,7 @@ export default class Search extends React.Component {
   componentWillReceiveProps(nextProps) {
     this.setState({ query: nextProps.params.query });
 
-    fetch(`/api/search/videos/${this.state.query}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    .then(jsonData => jsonData.json())
-    .then(data => console.log(data))
-    .catch(err => console.log('Error with search:', err));
+    this.renderSearchResults(nextProps.params.query);
   }
 
   render() {
