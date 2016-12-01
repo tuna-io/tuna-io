487e0249ea78eab8fea3244d8dd6f08994d042d4
Christopher Tham <chriswt91@gmail.com>
2016-11-30 16:10:23 -0800

    (feat) Create `Search` component and routing

---
+++ client/src/modules/Search.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class Search extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: props.params.query,
+    };
+  }
+
+  componentDidMount() {
+    console.log('this.query', this.state.query);
+  }
+
+  render() {
+    return (
+      <div>{this.state.query}</div>
+    )
+  }
+}
